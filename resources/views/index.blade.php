b8c397bc344beb2aa971bf4076058024c348cd48
Keith Tsubouchi <keith.tsubouchi@outlook.com>
2017-02-25 10:13:54 -0800

    Added bands table and datatables JS to get data.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,40 @@
 
 @section('content')
     <div class="container">
-        test
+        <span class="page-title">Bands</span>
+
+        <span class="page-options">
+            <a href="#"><i class="fa fa-plus"></i> Add Band</a>
+        </span>
+
+        <hr>
+
+        <table id="bands-grid" class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Start Date</th>
+                    <th>Website</th>
+                    <th>Still Active</th>
+                </tr>
+            </thead>
+        </table>
     </div>
 @endsection
+
+@push('js')
+<script>
+    $("#bands-grid")
+        .DataTable({
+            processing: false,
+            serverSide: false,
+            ajax: '{{ action('BandController@ajaxGetBands') }}',
+            columns: [
+                {data:'name'},
+                {data:'start_date'},
+                {data:'website'},
+                {data:'still_active'}
+            ]
+        })
+</script>
+@endpush
