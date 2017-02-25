28284f0ebbac29d6ae0cf5dd8b728decebf8dd26
Keith Tsubouchi <keith.tsubouchi@outlook.com>
2017-02-25 10:51:18 -0800

    Added related model method.

--- app/Album.php
+++ app/Album.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {
     protected $table = 'albums';
+	
+	/**
+	 * Get related App\Band
+	 * 
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function band()
+	{
+		return $this->belongsTo('App\Band');
+	}
 }
