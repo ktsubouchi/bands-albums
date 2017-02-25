e41077aa80883d7650da6907efca1735069ec937
Keith Tsubouchi <keith.tsubouchi@outlook.com>
2017-02-25 10:34:21 -0800

    AlbumController: created method to return album data to use with a Datatable.

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Album;
 use Illuminate\Http\Request;
+use Yajra\Datatables\Datatables;
 
 class AlbumController extends Controller
 {
-    //
+	/**
+	 * Gets albums and prepares them for use in a Datatable.
+	 */
+	public function ajaxGetAlbums()
+    {
+	    $albums = Album::all();
+	    
+	    return Datatables::of($albums)->make(true);
+    }
 }
