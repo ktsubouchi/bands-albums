ea71808961723b8b8851011d089888d3df745069
Keith Tsubouchi <keith.tsubouchi@outlook.com>
2017-02-25 10:18:35 -0800

    Band: added accessor for 'still_active'.

--- app/Band.php
+++ app/Band.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Band extends Model
 {
     protected $table = 'bands';
+	
+	/**
+	 * Get the band's current state.
+	 * 
+	 * @param $value
+	 *
+	 * @return string
+	 */
+	public function getStillActiveAttribute($value)
+	{
+		return $value == 0 ? 'No' : 'Yes';
+	}
 }
