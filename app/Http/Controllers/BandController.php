8f2d3b1d193175389e4fda59be49f5fd92d2590e
Keith Tsubouchi <keith.tsubouchi@outlook.com>
2017-02-25 09:53:32 -0800

    BandController: added method to return band data via ajax.

--- app/Http/Controllers/BandController.php
+++ app/Http/Controllers/BandController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Band;
 use Illuminate\Http\Request;
 
 class BandController extends Controller
 {
-    //
+	/**
+	 * Gets all bands and prepares them for use with a Datatable.
+	 * 
+	 * @return mixed
+	 */
+	public function ajaxGetBands()
+	{
+		$bands = Band::all();
+		
+		return Datatables::of($bands)->make(true);
+    }
 }
